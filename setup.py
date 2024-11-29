150b510131ac05787be39e90c592737b01d3831f
Junior Andrade <seisxis@gmail.com>
2024-11-29 15:53:08 -0300

    version

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ with open('README.rst') as f:
 
 setup(
     name='easysnmp',
-    version='0.2.5a',
+    version='0.2.6a',
     description='A blazingly fast and Pythonic SNMP library based on the '
                 'official Net-SNMP bindings',
     long_description=long_description,
